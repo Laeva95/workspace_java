31cb9e6958f0a8863613a7b1360c98049e4df587
laeva95 <dnskrdl123@gmail.com>
2026-07-30 11:46:52 +0900

    2026-07-30 수업내용 2

--- 22중첩클래스&인터페이스/src/sec07/exam02/HomeExample.java
+++ 22중첩클래스&인터페이스/src/sec07/exam02/HomeExample.java
@@ -7,6 +7,7 @@ interface RemoteControl{
 
 class Home implements RemoteControl{
 	// 익명 구현 객체
+	// [1] 전역 변수
 	private RemoteControl rc = new RemoteControl() {
 		
 		@Override
@@ -30,13 +31,59 @@ class Home implements RemoteControl{
 		
 	}
 	
+	public void use1() {
+		rc.turnOn();
+		rc.turnOff();
+	}
+	public void use2() {
+		// [2] 지역 변수
+		RemoteControl rc = new RemoteControl() {
+			
+			@Override
+			public void turnOn() {
+				System.out.println("에어컨의 전원을 켭니다.");
+			}
+			
+			@Override
+			public void turnOff() {
+				System.out.println("에어컨의 전원을 끕니다.");
+			}
+		};
+		
+		rc.turnOn();
+		rc.turnOff();
+	}
+	
+	public void use3(RemoteControl rc) {
+		rc.turnOn();
+		rc.turnOff();
+	}
+	
 }
 
 public class HomeExample {
 	
 	public static void main(String[] args) {
 		
+		Home home = new Home();
 		
+		home.use1();
+		
+		home.use2();
+		
+		// [3] 매개 변수
+		home.use3(new RemoteControl() {
+			
+			@Override
+			public void turnOn() {
+				System.out.println("보일러의 전원을 켭니다.");
+			}
+			
+			@Override
+			public void turnOff() {
+				System.out.println("보일러의 전원을 끕니다.");
+			}
+		});
 		
 	}
 }
