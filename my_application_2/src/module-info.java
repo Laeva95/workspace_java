202ce90b13392cc30ea92288d94bafc0101ab2d5
laeva95 <dnskrdl123@gmail.com>
2026-07-30 17:00:00 +0900

    2026-07-30 수업내용 5

---
+++ my_application_2/src/module-info.java
@@ -0,0 +1,6 @@
+
+// 다른 모듈 프로젝트에서 제공하는 패키지를 사용하기 위한 설정
+module my_application_2 {
+	requires my_module_a;
+	requires my_module_b;
+}
---
+++ my_module_a/src/module-info.java
@@ -0,0 +1,9 @@
+
+
+// module 모듈명 {}
+module my_module_a {
+	
+	// 모듈 프로젝트가 포함하고 있는 패키지를 외부에서 사용할 수 있도록 작성
+	exports pack1;
+	exports pack2;
+}
