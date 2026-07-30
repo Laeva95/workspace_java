6658d9c817a4c07cc34d6be1e766192061a1c2e5
laeva95 <dnskrdl123@gmail.com>
2026-07-30 17:44:27 +0900

    2026-07-30 수업내용 6

---
+++ my_application_3/src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+import pack1.*;
+import pack2.*;
+import pack3.*;
+import pack4.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		A a = new A(); a.method();
+		B b = new B(); b.method();
+		C c = new C(); c.method();
+		D d = new D(); d.method();
+		
+		
+	}
+}
